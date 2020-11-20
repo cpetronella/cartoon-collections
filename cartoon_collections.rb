9dfde8658d430447a774c78b4d53a1a1d9157971
Christopher <christopher.petronella@gmail.com>
2020-11-20 14:40:13 -0500

    commit tree 6447fb6485fa5aead2c8526cb61ebb47f0bd1e1a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, index| puts "#{index+1} #{dwarf}"}
+end
+  
+def summon_captain_planet(array)
+  array.collect {|word| word.capitalize + "!" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(array2)
+  array2.any? { |plant| plant.length > 4} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array3)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array3.find {|cheese| cheese_types.include?(cheese) }
 end
